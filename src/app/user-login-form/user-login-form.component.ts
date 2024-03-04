9039b7be7ff9b2543a36091aa7602555a0cb16f6
Kelly Scott <116105463+kellysdev@users.noreply.github.com>
2024-03-04 11:56:18 -0800

    Import OnInit, Input and implement into class.  Import MatDialogRef and SnackBar from Angular Material and inject into component with API.

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,10 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
+
+// will close the dialog on success
+import { MatDialogRef } from "@angular/material/dialog";
+
+// display notifications to the user
+import { MatSnackBar } from "@angular/material/snack-bar";
+
+// import backend API
+import { UserRegistrationService } from "../fetch-api-data.service";
 
 @Component({
   selector: "app-user-login-form",
   templateUrl: "./user-login-form.component.html",
   styleUrl: "./user-login-form.component.scss"
 })
-export class UserLoginFormComponent {
+export class UserLoginFormComponent implements OnInit {
+ @Input() userData = { Username: "", Password: "" };
 
+ constructor(
+  public fetchApiData: UserRegistrationService,
+  public dialogRef: MatDialogRef<UserLoginFormComponent>,
+  public snackBar: MatSnackBar ) { }
+
+  ngOnInit(): void { }
 }
