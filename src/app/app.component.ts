ec89c2ea712ff96a301d33bf21f97b236f317acc
Kelly Scott <116105463+kellysdev@users.noreply.github.com>
2024-03-04 13:00:09 -0800

    import Material SnackBar and inject into component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 import { UserRegistrationFormComponent } from "./user-registration-form/user-registration-form.component";
 
@@ -11,7 +12,9 @@ import { UserRegistrationFormComponent } from "./user-registration-form/user-reg
 export class AppComponent {
   title = "movie-Angular-client";
 
-  constructor(public dialog: MatDialog) { }
+  constructor(
+    public dialog: MatDialog, 
+    public snackBar: MatSnackBar) { }
 
   // open dialog when the signup button is clicked
   openUserRegistrationDialog(): void {
