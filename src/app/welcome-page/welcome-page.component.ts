4b753543672be0e6aa1a403661de1d5866896dfa
Kelly Scott <116105463+kellysdev@users.noreply.github.com>
2024-03-05 11:39:38 -0800

    Initialize component and import UserLoginForm and UserRegistrationForm components, add functions to open dialogs to these components.

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,10 +1,28 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+
+import { UserLoginFormComponent } from "../user-login-form/user-login-form.component";
+import { UserRegistrationFormComponent } from "../user-registration-form/user-registration-form.component";
 
 @Component({
   selector: "app-welcome-page",
   templateUrl: "./welcome-page.component.html",
   styleUrl: "./welcome-page.component.scss"
 })
-export class WelcomePageComponent {
+export class WelcomePageComponent implements OnInit {
+  constructor(public dialog: MatDialog) { }
+  ngOnInit(): void { }
 
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      width: "280px"
+    });
+  }
+
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      width: "280px"
+    });
+  }
+  
 }
