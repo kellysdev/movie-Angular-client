04d0de1c24adffae0d626c085a3cca8ce8a8ca46
Kelly Scott <116105463+kellysdev@users.noreply.github.com>
2024-03-05 11:06:29 -0800

    MovieCard component fetches all movies and sets them in an array, movies.

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FetchApiDataService } from "../fetch-api-data.service";
+import { subscribe } from "diagnostics_channel";
 
 @Component({
   selector: "app-movie-card",
@@ -6,5 +8,19 @@ import { Component } from "@angular/core";
   styleUrl: "./movie-card.component.scss"
 })
 export class MovieCardComponent {
+  movies: any[] = [];
+  constructor (public fetchApiData: FetchApiDataService) { }
+
+  ngOnInit(): void {
+    this.getMovies();
+  }
+
+  getMovies(): void {
+    this.fetchApiData.getAllMovies().subscribe((resp: any) => {
+      this.movies = resp;
+      console.log(this.movies);
+      return this.movies;
+    });
+  }
 
 }
