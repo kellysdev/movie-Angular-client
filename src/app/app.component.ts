44bb2d042b9624629d6073363e9b7b96f5fa8b04
Kelly Scott <116105463+kellysdev@users.noreply.github.com>
2024-03-05 11:41:46 -0800

    Remove code that has been added to WelcomePage component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,4 @@
 import { Component } from "@angular/core";
-import { MatDialog } from "@angular/material/dialog";
-import { MatSnackBar } from "@angular/material/snack-bar";
-
-import { UserRegistrationFormComponent } from "./user-registration-form/user-registration-form.component";
-import { UserLoginFormComponent } from "./user-login-form/user-login-form.component";
-import { MovieCardComponent } from "./movie-card/movie-card.component";
 
 @Component({
   selector: "app-root",
@@ -13,30 +7,5 @@ import { MovieCardComponent } from "./movie-card/movie-card.component";
 })
 export class AppComponent {
   title = "movie-Angular-client";
-
-  constructor(
-    public dialog: MatDialog, 
-    public snackBar: MatSnackBar) { }
-
-  // open dialog when the signup button is clicked
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-      // assign the dialog width
-      width: "280px"
-    });
-  }
-
-  // open dialog when the login button is clicked
-  openLoginDialog(): void {
-    this.dialog.open(UserLoginFormComponent, {
-      width: "280px"
-    });
-  }
-
-  openMoviesDialog(): void {
-    this.dialog.open(MovieCardComponent, {
-      width: "500px"
-    });
-  }
 
 }
