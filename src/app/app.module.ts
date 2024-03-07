509b86f9ce2fc60df5f98922804df949ea1fd91b
Kelly Scott <116105463+kellysdev@users.noreply.github.com>
2024-03-06 17:33:41 -0800

    Add toolbar above movies cards with app title, icon that when clicked goes to user profile, and a logout icon.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FormsModule } from "@angular/forms";
 import { RouterModule, Routes } from "@angular/router";
 
 import { MatInputModule } from "@angular/material/input";
+import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { MatFormFieldModule } from "@angular/material/form-field";
@@ -44,6 +45,7 @@ const appRoutes: Routes = [
     AppRoutingModule,
     FormsModule,
     MatInputModule,
+    MatToolbarModule,
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
