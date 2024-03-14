96ccc8db8b17ecc04cbcfbf7e5e3c16839c2b42c
Kelly Scott <116105463+kellysdev@users.noreply.github.com>
2024-03-14 10:12:02 -0700

    Attempt to implement Movie interface and fix setting of favoriteMovies.

--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 
+import { Movie } from "../models/movie.model";
 import { FetchApiDataService } from "../fetch-api-data.service";
 import { DataService } from "../data.service";
 
@@ -12,28 +13,8 @@ import { DataService } from "../data.service";
 export class ProfilePageComponent implements OnInit {
   userDetails: any = {}; // user object
   favoriteMovieIds: string[] = []; // array of favorite movie id's from user object
-  movies: object[] = []; // all movies
-  favoriteMovies: any = []; // favorite movie objects
-
-  @Input() movie = {
-    _id: "",
-    Title: "",
-    Description: "",
-    Genre: {
-      Name: "",
-      Description: ""
-    },
-    Director: {
-      Name: "",
-      Bio: "",
-      Birth: "",
-      Death: ""
-    },
-    ImagePath: "",
-    Featured: false,
-    Actors: [],
-    ReleaseDate: ""
-  };
+  movies: Movie[] = []; // all movies
+  favoriteMovies: any[] = []; // favorite movie objects
 
   constructor(
     public dialog: MatDialog,
@@ -53,24 +34,24 @@ export class ProfilePageComponent implements OnInit {
     this.fetchApiData.getSingleUser(username).subscribe((resp: any) => {
       this.userDetails = resp;
       this.favoriteMovieIds = this.userDetails.FavoriteMovies;
-      this.getFavoriteMovies();
-      // return this.userDetails
-    })
+      console.log(this.favoriteMovieIds);
+    });
   }
 
   getMovies(): void {
-    // fetch movies
+    // fetch all movies
     this.fetchApiData.getAllMovies().subscribe((resp: any) => {
       this.movies = resp;
-      // return this.movies;
+      console.log(this.movies);
     })
-  }
-
-  getFavoriteMovies(): void {
-    // return an array of objects that contains a movie object for each movie id string in the favoriteMovieIds array
+    // clear favoriteMovies array before repopulating
+    this.favoriteMovies = [];
+    // map each favoriteMovieId to the corresponding movie object
+    // populates an array of undefined for each item in favoriteMovieIds unless filer is applied
     this.favoriteMovies = this.favoriteMovieIds.map(favoriteMovieId => {
-      return this.movies.find(movie => this.movie._id === favoriteMovieId);
-    })
+      return this.movies.find(movie => movie._id === favoriteMovieId);
+    }).filter(movie => movie !== undefined);
+    console.log(this.favoriteMovies);
   }
 
 }
