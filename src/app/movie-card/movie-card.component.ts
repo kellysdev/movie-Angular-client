b115c1d4c081c216259453b780416cc6015b9ae0
Kelly Scott <116105463+kellysdev@users.noreply.github.com>
2024-03-14 07:20:06 -0700

    Update handling of adding a favorite movie and ngOnInit cleaned up and now sets favoriteMovies correctly.
    Also remove a console.log from testing director info rendering.

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -8,6 +8,7 @@ import { DataService } from "../data.service";
 import { DirectorViewComponent } from "../director-view/director-view.component";
 import { GenreViewComponent } from "../genre-view/genre-view.component";
 import { SynopsisViewComponent } from "../synopsis-view/synopsis-view.component";
+import { error } from "node:console";
 
 @Component({
   selector: "app-movie-card",
@@ -52,31 +53,28 @@ export class MovieCardComponent {
     const username: string = this.dataService.getUsername();
     this.fetchApiData.getSingleUser(username).subscribe((resp: any) => {
       this.userDetails = resp;
-      return this.userDetails;
+      this.username = resp.Username;
+      this.favoriteMovies = resp.FavoriteMovies || [];
+      this.isMovieInFavorites();
     })
 
     // set the username and favorite movies array
-    this.getDetails();
-
-    // set whether this movie is a favorite or not
-    this.isMovieInFavorites();
+    // this.getDetails();
     
   }
 
   // get the username and favorite movies array from the user object
-  getDetails() {
-    this.username = this.userDetails.Username;
-    this.favoriteMovies = this.userDetails.FavoriteMovies ?
-      this.userDetails.FavoriteMovies 
-      : [];
-    return this.username, this.favoriteMovies;
-  }
+  // getDetails() {
+  //   this.username = this.userDetails.Username;
+  //   this.favoriteMovies = this.userDetails.FavoriteMovies ?
+  //     this.userDetails.FavoriteMovies 
+  //     : [];
+  //   return this.username, this.favoriteMovies;
+  // }
 
   // determine whether this movie is in the favorite movies array
   isMovieInFavorites() {
-    if (this.favoriteMovies.length > 0 && this.favoriteMovies.includes(this.movie)) {
-      this.isThisAFavorite = true;
-    }
+    this.isThisAFavorite = this.favoriteMovies.includes(this.movie._id);
   }
 
   // open Director dialog and pass Director data to DirectorView component
@@ -85,7 +83,6 @@ export class MovieCardComponent {
       Director: this.movie.Director
     };
     this.dialog.open(DirectorViewComponent, this.dialogConfig);
-    // console.log(this.dialogConfig.data);
   }
 
   // open Genre dialog and pass Genre data to GenreView component
@@ -105,18 +102,19 @@ export class MovieCardComponent {
   }
 
   // add a movie to this users list of favorite movies
-  // get a 401 (unauthorized) error in console from POST request
   addToFavorites() {
-    this.fetchApiData.addFavoriteMovie(this.movie._id, this.userDetails).subscribe((result) => {
-      if (result) {
+    this.fetchApiData.addFavoriteMovie(this.movie._id, this.userDetails).subscribe({
+      next: (result => {
         this.isThisAFavorite = true;
-        console.log(this.favoriteMovies);
-        console.log(result);
         this.snackBar.open(this.movie.Title, "Added to Favorite Movies", {
           duration: 2000
         });
-      } else {
-        console.log(Error);
+      }),
+      error: (error) => {
+        console.log("Error adding to favorites:", error);
+        this.snackBar.open("An error occurred wile adding to favorites", "OK", {
+          duration: 2000
+        });
       }
     });
   }
