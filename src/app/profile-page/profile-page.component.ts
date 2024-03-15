84b7bf71c75b1764911d59f07b3b79a0fb99f5a9
Kelly Scott <116105463+kellysdev@users.noreply.github.com>
2024-03-15 11:33:34 -0700

    Filter all movies for favorite movies after fetch.  Remove unnecessary code.

--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 
-import { Movie } from "../models/movie.model";
 import { FetchApiDataService } from "../fetch-api-data.service";
 import { DataService } from "../data.service";
 
@@ -12,8 +11,6 @@ import { DataService } from "../data.service";
 })
 export class ProfilePageComponent implements OnInit {
   userDetails: any = {}; // user object
-  favoriteMovieIds: string[] = []; // array of favorite movie id's from user object
-  movies: Movie[] = []; // all movies
   favoriteMovies: any[] = []; // favorite movie objects
 
   constructor(
@@ -33,25 +30,14 @@ export class ProfilePageComponent implements OnInit {
     const username = this.dataService.getUsername();
     this.fetchApiData.getSingleUser(username).subscribe((resp: any) => {
       this.userDetails = resp;
-      this.favoriteMovieIds = this.userDetails.FavoriteMovies;
-      console.log(this.favoriteMovieIds);
     });
   }
 
   getMovies(): void {
-    // fetch all movies
+    // fetch all movies and return only favoriteMovies
     this.fetchApiData.getAllMovies().subscribe((resp: any) => {
-      this.movies = resp;
-      console.log(this.movies);
-    })
-    // clear favoriteMovies array before repopulating
-    this.favoriteMovies = [];
-    // map each favoriteMovieId to the corresponding movie object
-    // populates an array of undefined for each item in favoriteMovieIds unless filer is applied
-    this.favoriteMovies = this.favoriteMovieIds.map(favoriteMovieId => {
-      return this.movies.find(movie => movie._id === favoriteMovieId);
-    }).filter(movie => movie !== undefined);
-    console.log(this.favoriteMovies);
+      this.favoriteMovies = resp.filter((m: {_id: any}) => this.userDetails.FavoriteMovies.indexOf(m._id) >= 0);
+    });
   }
 
 }
