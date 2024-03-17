49cfd43d9909f5dd9c4afd611deb44427ebf0794
Kelly Scott <116105463+kellysdev@users.noreply.github.com>
2024-03-16 17:57:44 -0700

    Populate component with initial logic to delete a users account.

--- src/app/delete-account/delete-account.component.ts
+++ src/app/delete-account/delete-account.component.ts
@@ -1,10 +1,47 @@
-import { Component } from "@angular/core";
+import { Component, Inject, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+
+import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { MatSnackBar } from "@angular/material/snack-bar";
+
+import { FetchApiDataService } from "../fetch-api-data.service";
+import { DataService } from "../data.service";
 
 @Component({
   selector: "app-delete-account",
   templateUrl: "./delete-account.component.html",
   styleUrl: "./delete-account.component.scss"
 })
-export class DeleteAccountComponent {
+export class DeleteAccountComponent implements OnInit{
+  username: string = "";
 
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private router: Router,
+    public fetchApiData: FetchApiDataService,
+    public dataService: DataService,
+    public snackBar: MatSnackBar) {}
+
+  ngOnInit(): void {
+    // accept username data from ProfilePage component
+    this.username = this.data.Username;
+  }
+
+  // when the delete account button is clicked, delete the users account
+  // this will log them out and send them back to the welcome page
+  deleteAccount(): void {
+    this.fetchApiData.deleteUser(this.username).subscribe({
+      next: (result => {
+        console.log(result);
+        this.dataService.logout()
+        this.router.navigate(["welcome"]);
+      }),
+      error: (error) => {
+        console.log("Delete user error:", error);
+        this.snackBar.open("Something went wrong.", "Try again", {
+          duration: 2000
+        });
+      }
+    });
+  }
 }
