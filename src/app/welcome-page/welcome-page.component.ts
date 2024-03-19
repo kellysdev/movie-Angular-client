cef2cfc9cd5c01aced132e3ea187e35b410b55e5
Kelly Scott <116105463+kellysdev@users.noreply.github.com>
2024-03-18 19:22:47 -0700

    Implement guest login.

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+
 import { MatDialog } from "@angular/material/dialog";
+import { MatSnackBar } from "@angular/material/snack-bar";
+
+import { FetchApiDataService } from "../fetch-api-data.service";
+import { DataService } from "../data.service";
 
 import { UserLoginFormComponent } from "../user-login-form/user-login-form.component";
 import { UserRegistrationFormComponent } from "../user-registration-form/user-registration-form.component";
@@ -10,7 +16,19 @@ import { UserRegistrationFormComponent } from "../user-registration-form/user-re
   styleUrl: "./welcome-page.component.scss"
 })
 export class WelcomePageComponent implements OnInit {
-  constructor(public dialog: MatDialog) { }
+  userData: object = {
+    Username: "guest",
+    Password: "guest"
+  };
+
+  constructor(
+    private router: Router,
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar,
+    public fetchApiDataService: FetchApiDataService,
+    public dataService: DataService) {
+  }
+
   ngOnInit(): void { }
 
   openUserRegistrationDialog(): void {
@@ -23,6 +41,24 @@ export class WelcomePageComponent implements OnInit {
     this.dialog.open(UserLoginFormComponent, {
       width: "280px"
     });
+  }
+
+  guestLogin() {
+     this.fetchApiDataService.userLogin(this.userData).subscribe((result) => {
+      if (result.user) {
+        const username = JSON.stringify(result.user.Username);
+        this.dataService.setUsername(JSON.parse(username));
+        localStorage.setItem("token", result.token);
+        this.snackBar.open("Login successful!", "OK", {
+          duration: 2000
+        });
+        this.router.navigate(["movies"]);
+      } else {
+        this.snackBar.open("Something went wrong.", "Try again", {
+          duration: 2000
+        });
+      }
+     });
   }
   
 }
